e3f23b5bfbcfc2f39e0dc92effa6d968f56cc46c
Jeanie Suparman <jeanie.wu@hotmail.com>
2015-09-26 11:23:36 -0700

    just do it

--- app/src/main/java/patienceshyu/tamatimer/MainActivity.java
+++ app/src/main/java/patienceshyu/tamatimer/MainActivity.java
@@ -132,8 +132,14 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         } else if (heart2.alive) {
             heart2.die();
         } else if (heart3.alive) {
+<<<<<<< Updated upstream
             sprite.cooked();
             heart3.die();
+=======
+            heart3.die();
+            sprite.cooked();
+            countdown.setText("");
+>>>>>>> Stashed changes
         }
     }
 
